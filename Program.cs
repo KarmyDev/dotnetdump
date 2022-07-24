2cd7d6f245eb181cb3266db3549bcd02e6551db2
KarmyDev <kszlagk@users.noreply.github.com>
2022-07-25 00:15:40 +0200

    Added -f find --find

--- Program.cs
+++ Program.cs
@@ -13,8 +13,9 @@ namespace DotNetDumper
 		private static string ansiBrightBlue = "\u001b[34;1m";
 		private static string ansiYellow = "\u001b[33m";
 		private static string ansiBrightMagenta = "\u001b[35;1m";
+		private static string ansiBackgroundFind = "\u001b[40m";
 		
-		private static bool produceGraphviz;
+		private static bool produceGraphviz, findSpecificKeyword;
 		
         static void Main(string[] args)
         {
@@ -22,38 +23,48 @@ namespace DotNetDumper
 			
 			if (Array.IndexOf(args, "-r") > -1 || Array.IndexOf(args, "--raw") > -1) ansiReset = ansiRed = ansiBrightRed = ansiGreen = ansiBrightBlue = ansiYellow = ansiBrightMagenta = "";
 			if (Array.IndexOf(args, "-g") > -1 || Array.IndexOf(args, "--graph") > -1) produceGraphviz = true;
+			if (Array.IndexOf(args, "-f") > -1 || Array.IndexOf(args, "--find") > -1) findSpecificKeyword = true;
 			
 			if (!produceGraphviz) Console.WriteLine($"{ansiYellow}? {ansiReset}Attempting to load {ansiGreen}\"{args[0]}\"{ansiReset}...");
+			
+			string specificKeyword = "";
+			if (args.Length > 1) specificKeyword = args[2];
 			
 			string pathToAssembly = args[0];
 			
 			if (!File.Exists(pathToAssembly))
 			{
-				if (!produceGraphviz)Console.WriteLine($"{ansiRed}X File {ansiGreen}\"{args[0]}\"{ansiRed} doesn't exist.{ansiReset}");
+				if (!produceGraphviz) Console.WriteLine($"{ansiRed}X File {ansiGreen}\"{args[0]}\"{ansiRed} doesn't exist.{ansiReset}");
 				return;
 			}
 			
 			try 
 			{
 				Assembly externalDll = Assembly.LoadFrom(pathToAssembly);
-				if (!produceGraphviz)Console.WriteLine($"{ansiGreen}• {ansiReset}Loaded {ansiGreen}\"{externalDll.FullName}\"{ansiReset}!");
+				if (!produceGraphviz) Console.WriteLine($"{ansiGreen}• {ansiReset}Loaded {ansiGreen}\"{externalDll.FullName}\"{ansiReset}!");
 				
-				if (!produceGraphviz)Console.WriteLine($"{ansiYellow}? {ansiReset}Attempting to obtain types from {ansiGreen}\"{externalDll.FullName}\"{ansiReset}...");
+				if (!produceGraphviz) Console.WriteLine($"{ansiYellow}? {ansiReset}Attempting to obtain types from {ansiGreen}\"{externalDll.FullName}\"{ansiReset}...");
 				try 
 				{
 					Type[] externalDllTypes = externalDll.GetTypes();
-					if (!produceGraphviz)Console.WriteLine($"{ansiGreen}• {ansiReset}Successfully obtained types from {ansiGreen}\"{externalDll.FullName}\"{ansiReset}!");
+					if (!produceGraphviz)
+					Console.WriteLine($"{ansiGreen}• {ansiReset}Successfully obtained types from {ansiGreen}\"{externalDll.FullName}\"{ansiReset}!");
 					
-					if (!produceGraphviz)Console.WriteLine("\n");
+					if (!produceGraphviz)
+					Console.WriteLine("\n");
 
 					string graphHeader = $"digraph G {{\n\t{{\n\t\tbase [label=\"{externalDll.GetName().Name}.dll\", shape=\"folder\"]\n";
 					string graphBody = "";
 					
 					int typeCount = 0;
+					bool displayClassContent = false;
 					
 					foreach (Type type in externalDllTypes)
 					{
-						if (!produceGraphviz)Console.WriteLine($"Found Type: {ansiBrightMagenta}{type.Name}{ansiReset}");
+						if (!produceGraphviz && !findSpecificKeyword || type.Name.ToLower().Contains(specificKeyword.ToLower()))
+						Console.WriteLine($"Found Type: {ansiBrightMagenta + (findSpecificKeyword && type.Name.ToLower().Contains(specificKeyword.ToLower()) ? ansiBackgroundFind : "")}{type.Name}{ansiReset}");
+						
+						displayClassContent = type.Name.ToLower().Contains(specificKeyword.ToLower());
 						
 						string graphTableBuilder = "";
 						
@@ -70,7 +81,9 @@ namespace DotNetDumper
 								string memberEncodedName = System.Web.HttpUtility.HtmlEncode(member.Name);
 								graphTableBuilder += $"<FONT COLOR=\"{memberGraphColor}\">{memberEncodedName + memberSuffix}</FONT><BR/>";
 							}
-							if (!produceGraphviz)Console.WriteLine($"{ansiYellow}-- {ansiReset}Found {memberPrefix}: {memberColor}{member.Name}{ansiReset}{memberSuffix}");
+							
+							if (!produceGraphviz && !findSpecificKeyword || member.Name.ToLower().Contains(specificKeyword.ToLower()) || displayClassContent)
+							Console.WriteLine($"{ansiYellow}-- {ansiReset}Found {memberPrefix}: {memberColor + (findSpecificKeyword && member.Name.ToLower().Contains(specificKeyword.ToLower()) ? ansiBackgroundFind : "")}{member.Name}{ansiReset}{memberSuffix}");
 						}
 						
 						if (produceGraphviz) 
@@ -82,7 +95,7 @@ namespace DotNetDumper
 						
 						typeCount++;
 					}
-					if (!produceGraphviz) Console.WriteLine("\n");
+					if (!produceGraphviz && !findSpecificKeyword)  Console.WriteLine("\n");
 					
 					if (produceGraphviz) 
 					{
